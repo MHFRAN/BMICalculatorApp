bf57e8c0bb62a44e37622d40a1404bc325e90e7d
MHFRAN <kemsirve@gmail.com>
2024-06-12 22:48:02 +0200

    Primera subida

---
+++ app/src/main/java/com/mhfran/imcmobilecalculator/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mhfran.imcmobilecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mhfran.imcmobilecalculator.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // método al arrancar la pantalla
+    }
+}
