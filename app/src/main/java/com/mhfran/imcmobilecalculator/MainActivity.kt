bd61eb08ed590d4751fe4a4240f4352c14b7df44
MHFRAN <kemsirve@gmail.com>
2024-06-19 23:46:55 +0200

    Quinta actualización

--- app/src/main/java/com/mhfran/imcmobilecalculator/MainActivity.kt
+++ app/src/main/java/com/mhfran/imcmobilecalculator/MainActivity.kt
@@ -1,13 +1,66 @@
 package com.mhfran.imcmobilecalculator
 
-import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import android.os.Bundle
-import com.mhfran.imcmobilecalculator.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+    1. Ambas variables view no son accesibles desde fuera de esa clase
+    2. Lateinit es un modificador para variables no nulas (no aceptan NULL). La inicialización de la variable se
+       realizará posteriormente, útil cuando no se puede inicializar la variable en el momento de su declaración.
+    3. Var porque son variables que su valor puede cambiar después de iniciarse.
+    */
+
+    private var isMaleSelected:Boolean = true
+    private var isFemaleSelected:Boolean = false
+
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // método al arrancar la pantalla
+        initComponents()
+        initListeners() // son las funciones que están escuchando
+        initUI() // inicia la interfaz
+    }
+
+    /*
+    1. La función initComponents es un método privado que se encarga de inicializar las View.
+    2. findViewById es una función que busca una vista con el IPD especificado y devuelve una referencia.
+    3. Al estar las variables declaradas con el modificador "lateinit", deben inicializar antes de su uso.
+     */
+    private fun initComponents(){
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    // Cada vez que pulsemos en Las View llamaremos al método setGenderColor para cambiar el color del fondo
+    private fun initListeners() {
+       viewMale.setOnClickListener{ setGenderColor() }
+       viewFemale.setOnClickListener { setGenderColor() }
+    }
+
+    /* Llama a la función para saber cuál es el color que está seleccionado */
+    private fun setGenderColor(){
+        viewMale.setCardBackgroundColor(getBackGroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackGroundColor(isFemaleSelected))
+    }
+
+
+    /* Escogemos un color u otro dependiendo si el componente está o no seleccionado */
+    private fun getBackGroundColor(isSelectedComponent:Boolean):Int{
+        val colorReference = if(isSelectedComponent){
+            R.color.background_component_selected
+        }else {
+            R.color.background_component
+        }
+        return ContextCompat.getColor(this, colorReference)
+    }
+    /*  */
+    private fun initUI() {
+        TODO("Not yet implemented")
     }
 }
